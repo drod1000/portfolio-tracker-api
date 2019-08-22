0434d2e8bcd4b9734533aaa67d35f9a3c44012b0
Daniel Rodriguez <drod2793@gmail.com>
2019-08-22 18:31:42 -0500

    Removes db-migrate

--- migrations/sqls/20190818005451-CREATE-WATCHLISTSTOCK-up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE  WatchlistStock (
-  WatchlistStock INT AUTO_INCREMENT PRIMARY KEY,
-  StockId INT NOT NULL,
-  WatchDate DATE NOT NULL,
-  WatchPrice DECIMAL(6,2) NOT NULL,
-  CONSTRAINT FK_WatchlistStock_Stock_StockId FOREIGN KEY (StockId) REFERENCES Stock(StockId) ON DELETE CASCADE
-);
