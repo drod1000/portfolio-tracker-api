6634f3e32e3e1fd882d9894c12c813180e21533d
Daniel Rodriguez <drod2793@gmail.com>
2019-08-22 19:20:42 -0500

    Adds migration to add stock history

---
+++ migrations/20190822190932_create_stock_history.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('StockHistory', function(table) {
+    table.increments('StockHistory');
+    table.integer('StockId').notNullable().unsigned();
+    table.foreign('StockId').references('Stock.StockId');
+    table.date('RecordDate').notNullable();
+    table.decimal('OpenPrice').notNullable();
+    table.decimal('ClosePrice').notNullable();
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('StockHistory');
+};
