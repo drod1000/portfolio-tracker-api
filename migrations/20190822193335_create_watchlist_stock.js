dc2b835b4c2e324e8fbbfd0abd6c35d6847b603b
Daniel Rodriguez <drod2793@gmail.com>
2019-08-22 19:36:01 -0500

    Adds migration to add watchlist stock

---
+++ migrations/20190822193335_create_watchlist_stock.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('WatchlistStock', function(table) {
+    table.increments('WatchlistStockId');
+    table.integer('StockId').notNullable().unsigned();
+    table.foreign('StockId').references('Stock.StockId');
+    table.date('WatchDate').notNullable();
+    table.decimal('WatchPrice').notNullable();
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('WatchlistStock');
+};
