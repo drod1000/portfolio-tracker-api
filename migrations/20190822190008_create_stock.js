ed3477ac071fe06508adf4608c4f6a0f475dcba5
Daniel Rodriguez <drod2793@gmail.com>
2019-08-22 19:08:06 -0500

    Adds migration to add stock

---
+++ migrations/20190822190008_create_stock.js
@@ -0,0 +1,11 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('Stock', function(table) {
+    table.increments('StockId');
+    table.string('StockSymbol', 16);
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('Stock');
+};
