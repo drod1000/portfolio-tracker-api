4ee303d85e881b4508951ae6d21a78c8b1685b63
Daniel Rodriguez <drod2793@gmail.com>
2019-08-24 11:26:25 -0500

    Updates stock single-day-history endpoint with wtd service

--- api/routes/stock.js
+++ api/routes/stock.js
@@ -1,11 +1,36 @@
 import { Router } from 'express';
 
+import WorldTradingData from '../../services/world-trading-data';
+
 const router = Router();
 
 export default (app) => {
   app.use('/stock', router);
 
+  // TODO: Move this to a job
   router.get('/single-day-history', (req, res) => {
-    res.send('Stock Single Day History');
+    // TODO: Get from Stock DB;
+    const symbols = ["AAPL", "WFC"];
+    const worldTradingData = new WorldTradingData();
+
+    worldTradingData.getMultipleSingleDayHistory(symbols, req.query.date)
+      .then(data => {
+        const responseContent = Object.keys(data)
+          .map(symbol => {
+            return {
+              StockSymbol: symbol,
+              Price: data[symbol].close
+            };
+          });
+
+        res.status(200).send({
+          data: responseContent
+        });
+      })
+      .catch(error => {
+        res.status(400).send({
+          message: "Unable to retrieve data for date provided. Please confirm the market was open on that day."
+        })
+      })
   });
 }
