fdcd3b4335749eafb4042c0035135f6c358b0e10
Daniel Rodriguez <drod2793@gmail.com>
2019-08-23 20:05:17 -0500

    Enables esm

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-import express from 'express';
-
-async function startServer() {
-  const app = express();
-  await require('./initializers').default({ expressApp: app });
-
-  app.listen(3000, () => {
-    console.log('Listening on port 3000');
-  });
-}
-
-startServer();
+require = require("esm")(module/*, options*/)
+module.exports = require("./app.js")
