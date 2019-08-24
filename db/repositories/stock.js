4339fc97d6f546f2ea577468b30b32d723c676ca
Daniel Rodriguez <drod2793@gmail.com>
2019-08-24 17:31:28 -0500

    Adds stock repository

---
+++ db/repositories/stock.js
@@ -0,0 +1,12 @@
+class StockRepository {
+  constructor(container) {
+    this._knexConnection = container.get('knex.connection');
+  }
+
+  async getAllStocks() {
+    const result = this._knexConnection.select().from('Stock');
+    return result;
+  }
+}
+
+export default StockRepository;
