8cfefb0b7632b3a2aa8bf83fc2c0e2ef9f5a744a
Daniel Rodriguez <drod2793@gmail.com>
2019-08-23 19:47:08 -0500

    Adds knex initializer

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-// DB Connection
-import dotenv from 'dotenv';
-dotenv.config();
-import mysql from 'mysql';
-
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
-connection.connect(err => {
-  if (err) {
-    console.log('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('connected as id ' + connection.threadId);
-});
-
 import express from 'express';
 
 async function startServer() {
