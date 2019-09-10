e89fe48e6525758a0e0313f444d7ccccab60bc29
Daniel Rodriguez <drod2793@gmail.com>
2019-09-09 20:38:00 -0500

    Adds daily update service

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,8 @@
 import express from 'express';
 import "reflect-metadata";
 import { Container } from 'typedi/Container';
-import sgMail from '@sendgrid/mail';
-require('dotenv').config();
 
-import StockHistoryService from './services/stock-history';
-import SendgridService from './services/sendgrid';
+import DailyUpdateService from './services/daily-update';
 
 async function startServer() {
   const app = express();
@@ -20,28 +17,9 @@ async function refreshOnly() {
   const app = express();
   await require('./initializers').default({ expressApp: app });
 
-  console.log("Refresh starting");
-
-  const stockHistoryService = Container.get(StockHistoryService);
-  await stockHistoryService.refreshAll();
-
-  console.log("Refresh complete");
-
-  const sendgridService = Container.get(SendgridService);
-  const templateData = {
-    "EmailDate": "2018-09-01",
-    "Positions": [
-      { "PositionId":9, "Quantity":14, "BuyDate":"2018-10-02T05:00:00.000Z", "BuyPrice":161.53, "StockSymbol":"FB", "RecordDate":"2019-09-06T05:00:00.000Z", "CurrentPrice":187.49 },
-      { "PositionId":10,"Quantity":22, "BuyDate":"2018-11-21T06:00:00.000Z", "BuyPrice":70.01, "StockSymbol":"TGT", "RecordDate":"2019-09-06T05:00:00.000Z", "CurrentPrice":109.85 }
-    ],
-    "Watchlist": [
-      { "WatchlistStockId":1, "WatchDate":"2019-08-28T05:00:00.000Z", "WatchPrice":135.19, "StockSymbol":"PEP", "RecordDate":"2019-09-06T05:00:00.000Z", "CurrentPrice":137.37 },
-      { "WatchlistStockId":2, "WatchDate":"2019-08-28T05:00:00.000Z", "WatchPrice":136.4, "StockSymbol":"DIS", "RecordDate":"2019-09-06T05:00:00.000Z", "CurrentPrice":139.55 }
-    ]
-  };
-
-  console.log('About to send with service');
-  await sendgridService.sendTemplateMessage('d-4bc9d2c2900f47f2a87a4004829bece4', templateData);
+  const dailyUpdateService = Container.get(DailyUpdateService);
+  console.log("Sending update");
+  await dailyUpdateService.sendDailyUpdate();
 
   console.log('Exiting');
   process.exit();
